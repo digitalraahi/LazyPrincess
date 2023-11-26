2c5412d01beddabd807e32b6629c6c3dac9f753a
digitalraahi <135437639+digitalraahi@users.noreply.github.com>
2023-11-26 18:56:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -389,20 +389,16 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://{URL_SHORTENR_WEBSITE}/api'
-    params = {'api': URL_SHORTNER_WEBSITE_API,
-              'url': link,
+    url = f'https://{URL_SHORTENR_WEBSITE}/easy_api'
+    params = {'key': URL_SHORTNER_WEBSITE_API,
+              'link': link,
               }
 
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json()
-                if data["status"] == "success":
-                    return data['shortenedUrl']
-                else:
-                    logger.error(f"Error: {data['message']}")
-                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+                data = await response.text()
+                return data['shortenedUrl']
 
     except Exception as e:
         logger.error(e)
