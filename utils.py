6d12f0ea3422955eefe32c5968e47476af3e95a5
digitalraahi <135437639+digitalraahi@users.noreply.github.com>
2023-11-26 17:45:25 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -385,31 +385,29 @@ def humanbytes(size):
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
     
 async def get_shortlink(link):
-      https = link.split(":")[0]
-   if URL_SHORTENR_WEBSITE == "api.shareus.io":
-    url = f'https://{URL_SHORTENR_WEBSITE}/easy_api'
-    params = {
-        "key": URL_SHORTNER_WEBSITE_API,
-        "link": link,
-    }
-else:
-    # Your existing code for the other URL
-    https = "https"
-    link = link.replace("http", https)
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
     url = f'https://{URL_SHORTENR_WEBSITE}/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
 
-try:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-            data = await response.json()
-            return data['shortenedUrl']
-           
-except Exception as e:
-    logger.error(e)
-    return f'{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json()
+                if data["status"] == "success":
+                    return data['shortenedUrl']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+
+    except Exception as e:
+        logger.error(e)
+        return f'{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+
 
 
 def get_readable_time(seconds: int) -> str:
